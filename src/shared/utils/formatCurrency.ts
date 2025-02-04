d2aa4d5865b49e33a344afa7724bbef61b06932a
juliohdsv <ricksv92@gmail.com>
2025-02-04 17:33:19 -0300

    feat(utils): Format date and currency

---
+++ src/shared/utils/formatCurrency.ts
@@ -0,0 +1,11 @@
+const formatCurrency = (value: number) =>{
+  return value.toLocaleString(
+    "pt-br",
+    {
+      style: "currency",
+      currency: "BRL"
+    }
+  );
+};
+
+export default formatCurrency;
