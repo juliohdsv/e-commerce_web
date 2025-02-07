0ade3312c71e749bcf132aa49cabc5dffdd2eab0
juliohdsv <ricksv92@gmail.com>
2025-02-07 16:48:33 -0300

    feat(prisma): Able the package

--- src/domain/entities/Product.ts
+++ src/domain/entities/Product.ts
@@ -1,13 +1,25 @@
-export class Product{
-  constructor(
-    public id: number,
-    public title: string,
-    public description: string,
-    public category: string,
-    public price: number,
-    public rating: number,
-    public stock: number,
-    public images: string[],
-    public thumbnail: string[]
-  ){}
+import { uuid } from "uuidv4";
+
+export class Product {
+  public readonly id: string;
+  public title: string;
+  public description: string;
+  public category: string;
+  public price: number;
+  public rating: number;
+  public stock: number;
+  public images: string[];
+  public thumbnail: string[];
+
+  constructor(props: Omit<Product, "id">, id?: string) {
+    this.id = id ?? uuid();
+    this.title = props.title;
+    this.description = props.description;
+    this.category = props.category;
+    this.price = props.price;
+    this.rating = props.rating;
+    this.stock = props.stock;
+    this.images = props.images;
+    this.thumbnail = props.thumbnail;
+  }
 }
--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,7 +1,16 @@
+import { uuid } from "uuidv4";
+
 export class User{
-  constructor(
-    public id: number,
-    public name: string,
-    public email: string,
-  ){}
+
+  public readonly id: string;
+  public name: string;
+  public email: string;
+  public password: string;
+
+  constructor(props: Omit<User, "id">, id?: string){
+    this.id = id ?? uuid();
+    this.name = props.name;
+    this.email = props.email;
+    this.password = props.password;
+  }
 }
